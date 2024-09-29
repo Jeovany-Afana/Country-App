6c88248a6dba794685b9b5738c2f90d3f7f1ee4a
Jeovany-Afana <wilfrieddylan451@gmail.com>
2024-09-29 23:24:01 +0000

    Premier enregistrement du projet Country App

---
+++ index.js
@@ -0,0 +1,96 @@
+// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+//Ok
+
+const inputSearch = document.getElementById('inputSearch');
+const minToMax = document.getElementById('minToMax');
+const maxToMin = document.getElementById('maxToMin');
+const alpha = document.getElementById('alpha');
+const inputRange = document.getElementById('inputRange');
+const countriesContainer = document.querySelector('.countries-container');
+let countries = [];
+// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+
+async function getCountries() {
+  await
+     fetch('https://restcountries.com/v3.1/all')
+    .then((res) => res.json())
+    .then((data) => countries = data);
+    
+}
+
+function showCountries(numberOfCountries, filter_result) 
+{
+
+  console.log(countries);
+
+  if (filter_result === null) {
+
+    countriesContainer.innerHTML = countries
+    .sort((a, b) => a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase()))
+    .map((country) => 
+      `
+       <li class="card">
+        <img src="${country.flags.svg}">
+        <h3>${country.name.common}</h3>
+        <h4>Capitale: ${country.capital}</h4>
+        <h4>Population: ${country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</h4>
+        </li>
+
+      `
+    ).join('');
+    
+  } else {
+    
+    countries.length = numberOfCountries;
+    
+    countriesContainer.innerHTML = countries
+    .filter((country) => country.name.common.toUpperCase().includes(filter_result.toUpperCase()))
+    .sort((a, b)=> a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase()))
+    .map((country) =>
+      
+      
+      `
+      <li class="card">
+      <img src="${country.flags.svg}">
+      <h3>${country.name.common}</h3>
+      <h4>Capitale: ${country.capital}</h4>
+      <h4>Continent: ${country.continents}</h4>
+      <h4>Indépendant: ${country.independent ? "Oui" : "Non"}</h4>
+      <h4>Population: ${country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</h4>
+      </li>
+      `
+    ).join("");
+  }
+  }
+  
+  
+
+inputRange.addEventListener('input', (event)=>{
+
+  getCountries().then(()=> showCountries(event.target.value, inputSearch.value));
+});
+
+inputSearch.addEventListener('input', (event) =>{
+  
+getCountries().then(() => showCountries(inputRange.value, event.target.value));
+
+});
+
+getCountries().then(() => showCountries(inputRange.value, inputSearch.value));
+
+
+   
+
+// 3 - Passer les données à une variable
+
+// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+
+// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
+
+//coutry.name.includes(inputSearch.value);
+
+// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+
+// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+
+
