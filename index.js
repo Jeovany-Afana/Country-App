faca582c2e4322e994063f7af06d2d945a858125
Jeovany-Afana <wilfrieddylan451@gmail.com>
2024-09-30 19:10:46 +0000

    Version finale

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const maxToMin = document.getElementById('maxToMin');
 const alpha = document.getElementById('alpha');
 const inputRange = document.getElementById('inputRange');
 const countriesContainer = document.querySelector('.countries-container');
+const rangeValue = document.getElementById('rangeValue');
+let selectElement = document.querySelector('select');
+let selectedOption = "1";
 let countries = [];
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
 
@@ -18,7 +21,7 @@ async function getCountries() {
     
 }
 
-function showCountries(numberOfCountries, filter_result) 
+function showCountries(numberOfCountries, filter_result, searchValue) 
 {
 
   //console.log(countries);
@@ -26,13 +29,45 @@ function showCountries(numberOfCountries, filter_result)
   if (filter_result === null) {
 
     countriesContainer.innerHTML = countries
-    .sort((a, b) => a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase()))
+    .sort((a, b) => {
+       switch (searchValue) {
+        case "0":
+          return a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase());
+        
+          case "1": 
+         return b.name.common.toUpperCase().localeCompare(a.name.common.toUpperCase());
+
+
+          case "2":
+           return a.population - b.population;
+
+
+          case "3":
+           return b.population - a.population;
+
+
+          case "4":
+           return a.area - b.area;
+
+          
+          case "5":
+           return b.area - a.area;
+
+
+        default:
+          alert('Méthode de trie indisponible');
+          return 0;
+       }
+      })
     .map((country) => 
       `
        <li class="card">
         <img src="${country.flags.svg}">
         <h3>${country.name.common}</h3>
         <h4>Capitale: ${country.capital}</h4>
+        <h4>Continent: ${country.continents}</h4>
+        <h4>Superficie: ${country.area} Km²</h4>
+        <h4>Indépendant: ${country.independent ? "Oui" : "Non"}</h4>
         <h4>Population: ${country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</h4>
         </li>
 
@@ -45,7 +80,37 @@ function showCountries(numberOfCountries, filter_result)
     
     countriesContainer.innerHTML = countries
     .filter((country) => country.name.common.toUpperCase().includes(filter_result.toUpperCase()))
-    .sort((a, b)=> a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase()))
+    .sort((a, b) => {
+      switch (searchValue) {
+       case "1":
+         return a.name.common.toUpperCase().localeCompare(b.name.common.toUpperCase());
+       
+         case "2": 
+        return b.name.common.toUpperCase().localeCompare(a.name.common.toUpperCase());
+
+
+         case "3":
+          return a.population - b.population;
+
+
+         case "4":
+          return b.population - a.population;
+
+
+         case "5":
+          return a.area - b.area;
+
+         
+         case "6":
+          return b.area - a.area;
+        
+
+
+       default:
+         alert('Méthode de trie indisponible');
+         return 0;
+      }
+     })
     .map((country) =>
       
       
@@ -55,6 +120,8 @@ function showCountries(numberOfCountries, filter_result)
       <h3>${country.name.common}</h3>
       <h4>Capitale: ${country.capital}</h4>
       <h4>Continent: ${country.continents}</h4>
+      <h4>Superficie: ${country.area} Km²</h4>
+      <h4>Fuso horaire: ${country.timezones.join('  ,')}</h4>
       <h4>Indépendant: ${country.independent ? "Oui" : "Non"}</h4>
       <h4>Population: ${country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</h4>
       </li>
@@ -66,32 +133,50 @@ function showCountries(numberOfCountries, filter_result)
   
 
 inputRange.addEventListener('input', (event)=>{
+  if (parseInt(rangeValue.innerHTML) === 0) {
 
-  getCountries().then(()=> showCountries(event.target.value, inputSearch.value));
-});
-
+    countriesContainer.innerHTML = `<h2 style="text-align: center;">Aucun résultat</h2>`
+    
+  } else {
+    
+    showCountries(event.target.value, inputSearch.value, selectedOption);
+  }
+  });
+  
 inputSearch.addEventListener('input', (event) =>{
   
-getCountries().then(() => showCountries(inputRange.value, event.target.value));
+showCountries(inputRange.value, event.target.value, selectedOption);
 
 });
 
-getCountries().then(() => showCountries(inputRange.value, inputSearch.value));
+
+
+
+selectElement.addEventListener('change', () => {
+  selectedOption = selectElement.options[selectElement.selectedIndex].value;
+  
+  showCountries(inputRange.value, inputSearch.value, selectedOption);
+});
+
+
+
+
+getCountries().then(() => showCountries(inputRange.value, inputSearch.value, selectedOption));
 
 
    // 2. Ensuite, enregistre le Service Worker pour la fonctionnalité hors ligne
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker
-          .register('./service-worker.js')
-          .then(registration => {
-              console.log('Service Worker enregistré avec succès', registration);
-          })
-          .catch(error => {
-              console.log('Service Worker échec de l\'enregistrement:', error);
-          });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//       navigator.serviceWorker
+//           .register('./service-worker.js')
+//           .then(registration => {
+//               console.log('Service Worker enregistré avec succès', registration);
+//           })
+//           .catch(error => {
+//               console.log('Service Worker échec de l\'enregistrement:', error);
+//           });
+//   });
+// }
 
 // 3 - Passer les données à une variable
 
@@ -103,6 +188,4 @@ if ('serviceWorker' in navigator) {
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
-
-
+// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pay
