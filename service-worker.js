041cd43c5a5116d8e0843189c47b0df5ec81ffa2
Jeovany-Afana <wilfrieddylan451@gmail.com>
2024-09-30 14:02:29 +0000

    Ajout du fichier manifest.json pour gérer l'installation de l'application dans les téléphones

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,15 @@ const CACHE_NAME = 'country-app-cache-v1';
 
 // Les fichiers à mettre en cache
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './index.js',
-  './countries.json', // Si tu utilises un fichier JSON pour les données
-  './service-worker.js'
+ './',                    // Cache la racine (index.html par défaut)
+  './index.html',           // Cache ton fichier HTML
+  './style.css',            // Cache ton fichier CSS
+  './index.js',             // Cache ton fichier JS principal
+  './service-worker.js',    // Cache le Service Worker lui-même
+  './countries.json',
+  './manifest.json',        // Cache le manifest pour PWA
+  './icons/logo1.jpeg', // Cache les icônes
+  './icons/official_logo.jpg'
 ];
 
 // Installer le Service Worker et mettre en cache les fichiers
